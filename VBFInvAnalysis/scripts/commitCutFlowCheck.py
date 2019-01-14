0227813873f7dfc759053f9bfdfeaf5d389718b6
Doug Schaefer <schae@uct3-s1.mwt2.org>
2019-01-14 11:45:01 -0600

    add cutflow check

--- VBFInvAnalysis/scripts/commitCutFlowCheck.py
+++ VBFInvAnalysis/scripts/commitCutFlowCheck.py
@@ -1,5 +1,21 @@
 import ROOT
 import sys
+ROOT.gROOT.SetBatch(True)
+
+def Diff(t_old, t_new, mvar='baseel_pt'):
+
+    mpt=ROOT.TH1F("mpt","mpt",100,0.0,100)
+    t_old.Draw('%s>>mpt' %mvar)
+    a=mpt.Integral(0,102)
+    t_new.Draw('%s>>mpt' %mvar)
+    b=mpt.Integral(0,102)
+    tdiff=1.0
+    if a!=0.0:
+        tdiff = (a-b)/a
+    print 'Old: %s New: %s Ratio: %0.3f' %(a,b, tdiff)
+    if abs(tdiff)>0.5:
+        print 'Cutflow differs by more than 0.5\%. Exiting'
+        sys.exit(0)
 
 def Compare(raw_old,raw_new):
 
@@ -15,8 +31,8 @@ def Compare(raw_old,raw_new):
             print 'Cut order changed. exiting...failure'
             sys.exit(0)
         print 'Cut: %s             %0.1f %0.1f            Diff:  %0.1f Ratio:  %0.3f' %(raw_old.GetXaxis().GetBinLabel(i), raw_old.GetBinContent(i), raw_new.GetBinContent(i), diff_ratio, ratio)
-        if abs(ratio)>0.005:
-        #if abs(ratio)>0.5:
+        #if abs(ratio)>0.005:
+        if abs(ratio)>0.5:
             print 'Cutflow differs by more than 0.5\%. Exiting'
             sys.exit(0)
 
@@ -43,6 +59,28 @@ w_old = fold.Get('cutflowhisto_Nominal')
 w_new = fnew.Get('cutflowhisto_Nominal')
 print 'Weighted counts!'
 Compare(w_old, w_new)
+
+#
+# Load the tree to check jets, electrons and muons
+#
+t_old = fold.Get('MiniNtuple')
+t_new = fnew.Get('MiniNtuple')
+print 'Counts of elec, muon, jets! Reading the MiniNtuple'
+
+print 'Base Electron'
+Diff(t_old, t_new, mvar='baseel_pt')
+
+print 'Base Muon'
+Diff(t_old, t_new, mvar='basemu_pt')
+
+print 'Signal Muon'
+Diff(t_old, t_new, mvar='mu_pt')
+
+print 'Signal Electron'
+Diff(t_old, t_new, mvar='el_pt')
+
+print 'Signal Jets'
+Diff(t_old, t_new, mvar='jet_pt')
 
 fnew.Close()
 fold.Close()
