9b911624a24b4c21b15cba4c90cf59a54198f47c
Othmane Rifki <othmane.rifki@cern.ch>
2019-02-20 16:26:49 +0100

    New algorithm to perform V+jets reweighting using theory input

---
+++ VBFInvAnalysis/VBFInvAnalysis/VBFInvVjetsRW.h
@@ -0,0 +1,100 @@
+#ifndef VBFInvAnalysis_VBFInvVjetsRW_H
+#define VBFInvAnalysis_VBFInvVjetsRW_H
+
+#include <EventLoop/Algorithm.h>
+//#include "xAODEventInfo/EventInfo.h"
+#include <AsgTools/AnaToolHandle.h>
+
+#include <PathResolver/PathResolver.h>
+
+#include <xAODCore/ShallowCopy.h>
+#include <xAODTruth/TruthParticleContainer.h>
+#include <xAODJet/JetContainer.h>
+
+#include <VBFInvAnalysis/contentHolder.h>
+#include <VBFInvAnalysis/VBFInv_Defs.h>
+//#include <VBFInvAnalysis/OutputTruth.h>
+#include <VBFInvAnalysis/CutFlowTool.h>
+
+class VBFInvVjetsRW : public EL::Algorithm {
+public:
+
+   // put your configuration variables here as public variables.
+   // that way they can be set directly from CINT and python.
+public:
+   TString outputName;
+
+   // variables that don't get filled at submission time should be
+   // protected from being send from the submission node to the worker
+   // node (done by the //!)
+public:
+   // things used to access xAOD information
+   Bool_t m_determinedDerivation; //!
+   Bool_t m_isDAODTRUTH; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_particles; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_muons; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_photons; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_electrons; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_bosons; //!
+   std::pair<xAOD::TruthParticleContainer*, xAOD::ShallowAuxContainer*> m_neutrinos; //!
+
+   // things used for output
+   TTree * m_tree; //!
+   //   Analysis::OutputTruth m_cand; //!
+   TH1F *m_histoEventCount; //!
+
+   // things used in the analysis
+   Bool_t m_iso_dyn; //!
+   Int_t m_run; //!
+   Int_t m_event; //!
+   Int_t m_eventCounter; //!
+   Float_t m_mconly_weight; //!
+   Float_t m_n_partonLeptons; //!
+   Float_t m_n_partonNeutrinos; //!
+   Float_t m_n_partonPhotons; //!
+   Analysis::CutFlowTool m_cutFlow; //!
+   std::map < TString, Bool_t > m_iso; //!
+   std::vector< Float_t > m_truth_mc_px; //!
+   std::vector< Float_t > m_truth_mc_py; //!
+   std::vector< Float_t > m_truth_mc_pz; //!
+   std::vector< Float_t > m_truth_mc_e; //!
+   std::vector< Float_t > m_truth_mc_px_dressed; //!
+   std::vector< Float_t > m_truth_mc_py_dressed; //!
+   std::vector< Float_t > m_truth_mc_pz_dressed; //!
+   std::vector< Float_t > m_truth_mc_e_dressed; //!
+   std::vector< Int_t > m_truth_mc_barcode; //!
+   std::vector< Int_t > m_truth_mc_status; //!
+   std::vector< Int_t > m_truth_mc_type; //!
+   std::vector< Int_t > m_truth_mc_origin; //!
+   std::vector< Int_t > m_truth_mc_pdg; //!
+   std::vector< Int_t > m_truth_mc_dyn_iso; //!
+   std::vector< Int_t > m_truth_mc_fix_iso; //!
+
+   // this is a standard constructor
+   VBFInvVjetsRW();
+
+   // methods defined in the analysis
+   virtual EL::StatusCode analyzeEvent();
+   virtual EL::StatusCode resetBranches();
+   virtual EL::StatusCode defineContainers();
+   virtual EL::StatusCode keepEvent();
+   virtual EL::StatusCode fillParticle(xAOD::TruthParticle* thisParticle);
+   Bool_t selectMCTruthParticle(xAOD::TruthParticle* thisParticle);
+   virtual EL::StatusCode checkPhotonIsolation(xAOD::TruthParticle* thisParticle);
+
+   // these are the functions inherited from Algorithm
+   virtual EL::StatusCode setupJob(EL::Job& job);
+   virtual EL::StatusCode fileExecute();
+   virtual EL::StatusCode histInitialize();
+   virtual EL::StatusCode changeInput(bool firstFile);
+   virtual EL::StatusCode initialize();
+   virtual EL::StatusCode execute();
+   virtual EL::StatusCode postExecute();
+   virtual EL::StatusCode finalize();
+   virtual EL::StatusCode histFinalize();
+
+   // this is needed to distribute the algorithm to the workers
+   ClassDef(VBFInvVjetsRW, 1);
+};
+
+#endif
--- VBFInvAnalysis/scripts/runVBF.py
+++ VBFInvAnalysis/scripts/runVBF.py
@@ -24,7 +24,7 @@ parser.add_argument( "-k", "--nskip", type = int, dest = "nskip", help="Number o
 parser.add_argument( "-r", "--replicationSite", type = str, dest = "replicationSite", default = "DESY-HH_LOCALGROUPDISK", help="Name of disk where to replicate output of grid jobs" )
 parser.add_argument( "-w", "--overwrite", dest = "overwrite", action = "store_false", default = True, help = "don't overwrite previous submitDir")
 parser.add_argument( "-d", "--driver", type = str, dest = "driver", default = "local", choices = ["local", "prun","condor"], help = "driver to be used (local, prun, condor)", metavar="driver")
-parser.add_argument( "-a", "--algo", type = str, dest = "algoName", default = "VBFInv", choices = ["VBFInv","VBFInvTruth", "VBFInvSherpaTruth"], help = "algorithm name (e.g. VBFInv, VBFInvTruth)")
+parser.add_argument( "-a", "--algo", type = str, dest = "algoName", default = "VBFInv", choices = ["VBFInv","VBFInvTruth", "VBFInvSherpaTruth", "VBFInvVjetsRW"], help = "algorithm name (e.g. VBFInv, VBFInvTruth, VBFInvSherpaTruth, VBFInvVjetsRW)")
 parser.add_argument( "-u", "--user", type=str, dest="userName", default=os.environ["USER"], help="username for grid jobs", metavar="userName")
 
 # Algorithm configuration
@@ -192,7 +192,7 @@ if( args.algoName == "VBFInv" ):
   alg.doMETDetail = args.doMETDetail or args.doDetail
   alg.doEventDetail = args.doEventDetail or args.doDetail
   alg.doContLepDetail = args.doContLepDetail or args.doDetail
-  alg.savePVOnly = False # saves only the PV value for the jets when True  
+  alg.savePVOnly = False # saves only the PV value for the jets when True
   alg.doRnS = args.doRnS
   alg.doFatJetDetail = args.doFatJetDetail
   alg.doTrackJetDetail = args.doTrackJetDetail
@@ -207,8 +207,10 @@ elif ( args.algoName == "VBFInvSherpaTruth"):
   alg.shouldNotCluster = args.noClusterPartons
   alg.partonJetPtCut = args.partonPt
   alg.truthJetPtCut = args.truthPt
+elif ( args.algoName == "VBFInvVjetsRW"):
+  alg.skipCBK = args.skipCutBookKeper
 else:
-  print("ERROR: You need to enter a valid algorithm name: \"VBFInv\" or \"VBFInvTruth\"")
+  print("ERROR: You need to enter a valid algorithm name: \"VBFInv\" or \"VBFInvTruth\" or \"VBFInvSherpaTruth\" or \"VBFInvVjetsRW\"")
   sys.exit()
 job.algsAdd( alg )
 
